6c03e813ee3709a44fb7c9094df0fcc47aacc5c9
cnhemiya <cnhemiya@qq.com>
2024-04-09 12:44:44 +0800

    v0.4.0

--- plugin.gd
+++ plugin.gd
@@ -10,8 +10,12 @@ func _enter_tree():
 	# Initialization of the plugin goes here.
 	add_custom_type("BmmInputMove3D", "Node", 
 			preload("bmm/bmm_input_move/bmm_input_move_3d.gd"), preload(_bmm_3d_icon))
+	add_custom_type("BmmInputMove2D", "Node", 
+			preload("bmm/bmm_input_move/bmm_input_move_2d.gd"), preload(_bmm_2d_icon))
 
 
 func _exit_tree():
 	# Clean-up of the plugin goes here.
 	remove_custom_type("BmmInputMove3D")
+	remove_custom_type("BmmInputMove2D")
+	
