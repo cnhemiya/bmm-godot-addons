a0c1499041f399c1acbed1c4f78874240a0e3e59
cnhemiya <cnhemiya@qq.com>
2024-04-21 23:25:39 +0800

    完善 跟随队列

--- bmm/bmm_gds/bmm_follow_queue/bmm_follow_queue.gd
+++ bmm/bmm_gds/bmm_follow_queue/bmm_follow_queue.gd
@@ -15,15 +15,8 @@
 class_name BmmFollowQueue
 extends RefCounted
 ## 这个类用于管理跟随队列，head 是领头节点，team 是跟随节点。
-## ** 未完成，有缺陷 **
+## 目前移动实现是“瞬移”方式
 
-
-# 领头节点
-var head: BmmNodeArea2D:
-	get:
-		return head
-	set(value):
-		head = value
 
 # 领头位移偏移
 var head_offset: float = 5.0:
@@ -32,6 +25,23 @@ var head_offset: float = 5.0:
 	set(value):
 		head_offset = value
 
+# 节点最小间距
+var min_distance: float = 128.0:
+	get:
+		return min_distance
+	set(value):
+		min_distance = value
+
+# team.back().index 到 path.size - 1 的数量
+# 达到这个阈值就重置 path 大小 为 team.back().index + 1
+var extra_path_size: int = 500
+
+# 领头节点
+var head: BmmNodeArea2D:
+	get:
+		return head
+	set(value):
+		head = value
 
 # 跟随节点
 var team: Array[BmmNodeAreaPath2D]:
@@ -41,7 +51,7 @@ var team: Array[BmmNodeAreaPath2D]:
 		team = value
 
 # 记录的路径
-var path: Array[BmmAreaPoint2D]:
+var path: Array[Vector2]:
 	get:
 		return path
 	set(value):
@@ -50,32 +60,61 @@ var path: Array[BmmAreaPoint2D]:
 
 # 添加领头节点路径
 func _append_head_path():
-	path.push_front(BmmAreaPoint2D.new(head.node))
+	path.push_front(head.node.position)
+	_update_team_path_index(1)
 
 
 # 更新 head 路径
 func update_head_path():
 	if path.is_empty():
 		_append_head_path()
-	elif path.front().position.distance_to(head.node.position) > head_offset:
+	elif path.front().distance_to(head.node.position) > head_offset:
 		_append_head_path()
 
 
 # 添加跟随节点路径
-func _append_team_path(area: BmmNodeArea2D) -> int:
-	path.push_back(BmmAreaPoint2D.new(area.node))
+func _append_team_path(node: Node2D) -> int:
 	return path.size() - 1
 
 
 # 添加跟随节点
 func append_team_node(node: Node2D, rect: Rect2):
 	var team_area = BmmNodeArea2D.new(node, rect)
-	var last: int = _append_team_path(team_area)
+	var last: int = _append_team_path(node)
 	team.append(BmmNodeAreaPath2D.new(team_area, last))
 
 
+# 更新跟随节点的路径索引
+func _update_team_path_index(offset: int = 1):
+	for i in team:
+		i.index += offset
 
 
+# 更新跟随节点路径
+func update_team_path():
+	if team.is_empty() or path.size() < 2:
+		return
+	var target_pos = path.front()
+	for t_i in team:
+		if path[t_i.index].distance_to(target_pos) <= min_distance + head_offset:
+			target_pos = path[t_i.index]
+		else:
+			for p_idx in range(t_i.index, 1, -1):
+				if path[p_idx - 1].distance_to(target_pos) <= min_distance + head_offset:
+					target_pos = path[p_idx]
+					t_i.index = p_idx
+					t_i.area.node.position = path[p_idx]
+					break
+
+
+# 重置 path 的 size
+func reset_path_size():
+	if (not path.is_empty()) and \
+			((path.size() - 1 - team.back().index) >= extra_path_size):
+		path.resize(team.back().index + 1)
+
+
+# 记录 NodeArea2D 和所在 path 的索引
 class BmmNodeAreaPath2D extends RefCounted:
 	var area: BmmNodeArea2D
 	var index: int
@@ -83,4 +122,3 @@ class BmmNodeAreaPath2D extends RefCounted:
 	func _init(_area: BmmNodeArea2D, _index: int):
 		area = _area
 		index = _index
-
